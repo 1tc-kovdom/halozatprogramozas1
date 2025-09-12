75bf04917e6a58c8dcde29554092c13b261202c1
1tc-kovdom <kovacsdominik434@gmail.com>
2025-09-12 12:36:09 +0200

    szorzotabla

--- ciklusok.py
+++ ciklusok.py
@@ -18,3 +18,30 @@ def parosszamok(lista):
 print(f"A lista elemeinek összege: {osszegez(lista)}")
 
 print(f"A lista páros elemeinek darabszáma: {parosszamok(lista)}")
+
+# Irass ki egy sorba 5 *-ot
+
+print("*"*5)
+
+for i in range(5):
+    print("*", end="")
+print()
+
+# rajzolj piramist:
+
+sor=5  
+for i in range(sor+1):
+    hely=' '*(sor-i)
+    csillag='*'*(2*i-1)
+    print(hely+csillag)
+
+# csinálj szorzótáblát 10*10
+
+for i in range (1, 11):
+    for j in range (1,  11):
+        print(f"{i*j:3}", end=" ")
+
+    print()
+
+
+    
