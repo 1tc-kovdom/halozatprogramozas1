f0db04cb245e162f6adbe86fd04e498e109fe599
1tc-kovdom <kovacsdominik434@gmail.com>
2025-09-19 13:12:52 +0200

    input

--- input.py
+++ input.py
@@ -10,11 +10,19 @@
 # print(f"A bekér számok összege: {osszeg}")
 # Enter végjelig kérjünk be számokat
 
-osszeg=0
-while True:
-    be = input("Kérem a számot, enter megnyomávásal kilépsz. : ")
-    if be=="":
-        break 
-    osszeg+= int(be)
+# osszeg=0
+# while True:
+#     be = input("Kérem a számot, enter megnyomávásal kilépsz. : ")
+#     if be == "":
+#         break 
+#     osszeg += int(be)
 
-print(f"Az összegük: {osszeg}")
+# print(f"Az összegük: {osszeg}")
+
+# kérj be egy számot 0 tól az utolsó számig irja ki a páros számokat
+hatar = int(input("Kérem a számot: "))
+posszeg=0
+while posszeg!=hatar+1:
+    if posszeg % 2==0:
+        print(posszeg)
+    posszeg+=1
