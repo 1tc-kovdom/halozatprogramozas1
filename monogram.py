917b8160e1bafcd5e4f0646377721e9d4bcdaf18
1tc-kovdom <kovacsdominik434@gmail.com>
2025-09-26 12:24:31 +0200

    monogramjav

--- monogram.py
+++ monogram.py
@@ -1,9 +1,17 @@
 # név bekérése, adja vissza a monogramját a névnek
-nev=input("Kérem a nevet: ")
-szavak=nev.split()
-monogram=""
-for szo in szavak:
-    if szo:  
+kettobetus = ["sz", "ty", "gy", "ny", "cs", "dz", "ly", "zs"]
+harombetus = ["dzs"]
+
+nev = input("Kérem a nevet: ")
+nevek = nev.split()
+monogram = ""
+
+for szo in nevek:
+    if szo[:3] in harombetus:
+        monogram+=szo[:3].capitalize()
+    elif szo[:2] in kettobetus:
+        monogram+=szo[:2].capitalize()
+    else:
         monogram+=szo[0].upper()
-        
+
 print("A monogramod:", monogram)
